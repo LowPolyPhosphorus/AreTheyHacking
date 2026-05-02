afe02aaa32703a182c97c9def3ea99dc024a52cb
lowpolyphosphorus <158785409+LowPolyPhosphorus@users.noreply.github.com>
2026-05-02 13:57:43 -0700

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,31 +1,34 @@
-const fs = require("fs");
-const path = require("path");
+const axios = require("axios");
 
-const STORE_FILE = path.join(__dirname, "../tokens.json");
+const JSONBIN_URL = `https://api.jsonbin.io/v3/b/${process.env.JSONBIN_BIN_ID}`;
+const HEADERS = {
+  "X-Master-Key": process.env.JSONBIN_API_KEY,
+  "Content-Type": "application/json",
+};
 
-function load() {
-  if (!fs.existsSync(STORE_FILE)) return {};
-  return JSON.parse(fs.readFileSync(STORE_FILE, "utf8"));
+async function load() {
+  const res = await axios.get(JSONBIN_URL, { headers: HEADERS });
+  return res.data.record ?? {};
 }
 
-function save(data) {
-  fs.writeFileSync(STORE_FILE, JSON.stringify(data, null, 2));
+async function save(data) {
+  await axios.put(JSONBIN_URL, data, { headers: HEADERS });
 }
 
 async function getToken(slackUserId) {
-  const store = load();
+  const store = await load();
   return store[slackUserId]?.access_token ?? null;
 }
 
 async function saveToken(slackUserId, accessToken, hackatimeUser) {
-  const store = load();
+  const store = await load();
   store[slackUserId] = {
     access_token: accessToken,
     hackatime_id: hackatimeUser?.id ?? null,
     slack_id: hackatimeUser?.slack_id ?? null,
     registered_at: new Date().toISOString(),
   };
-  save(store);
+  await save(store);
 }
 
 module.exports = { getToken, saveToken };
